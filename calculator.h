8f38006511e52ce95d3f649017fb42ef076b82ef
JackRybnikov <48389211+JackRybnikov@users.noreply.github.com>
2020-09-22 15:21:48 +0300

    Add files via upload

--- calculator.h
+++ calculator.h
@@ -1,11 +1,6 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
-
 class Calculator
 {
     public:
-        int Add (double, double);
-        int Sub (double, double);
+        static int Add (int, int);
+        static int Mul (double, double);
 };
-
-#endif//CALCULATOR_H
